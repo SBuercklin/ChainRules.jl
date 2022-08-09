290fd56e095298daf06c6122427702c01de35311
Sam Buercklin <SBuercklin@users.noreply.github.com>
2022-08-09 16:53:23 -0400

    Determine zero for fill from eltype(dy)
    In `_zerolike_writeat`, use `zero(eltype(dy))` rather than `0` to `fill!` the `dx` array. This allows us to work with numerical types `T` where `0 != zero(T)`

--- src/rulesets/Base/array.jl
+++ src/rulesets/Base/array.jl
@@ -539,7 +539,7 @@ end
 function _zerolike_writeat(x::AbstractArray{<:Number}, dy, dims, inds...)
     # It's unfortunate to close over `x`, but `similar(typeof(x), axes(x))` doesn't 
     # allow `eltype(dy)`, nor does it work for many structured matrices.
-    dx = fill!(similar(x, eltype(dy), axes(x)), 0)
+    dx = fill!(similar(x, eltype(dy), axes(x)), zero(eltype(dy)))
     view(dx, inds...) .= dy  # possibly 0-dim view, allows dy::Number and dy::Array, and dx::CuArray
     dx
 end
